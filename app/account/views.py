9fa7905d3b538a9781d3df1e57051a6730f814f5
fawn <297032113@qq.com>
2018-10-23 17:03:24 +0800

    初步完成登录登出（未测试）

--- app/account/views.py
+++ app/account/views.py
@@ -1,9 +1,36 @@
-from rest_framework import viewsets
+import json
 
-from app.account.models import RealUser
-from app.account.serializers import RealUserSerializer
+from django.contrib.auth import authenticate, login, logout
+from django.http import JsonResponse
 
 
-class RealUserViewSet(viewsets.ModelViewSet):
-    queryset = RealUser.objects.all()
-    serializer_class = RealUserSerializer
+def log_in(request):
+    """
+    账号登录
+    :param request:
+    :return: json {'is_login': True, 'message': ''}
+    """
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    message = {'is_login': False, 'message': ''}
+    if user is not None:
+        login(request, user)
+        message['is_login'] = True
+    else:
+        message['message'] = '登录失败'
+    message = json.dumps(message)
+    return JsonResponse(message)
+
+
+def log_out(request):
+    """
+    账号登出
+    :param request:
+    :return:
+    """
+    logout(request)
+    return JsonResponse(json.dumps({
+        'is_logout': True,
+        'message': '登出成功'
+    }))
